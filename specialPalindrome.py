26d160d51cfc35894a5e173a72f8ce2ceac8b566
Michael Moyles <micmoyles@gmail.com>
2018-11-01 10:12:55 +0000

    add notes

--- specialPalindrome.py
+++ specialPalindrome.py
@@ -3,6 +3,10 @@
 https://www.hackerrank.com/challenges/special-palindrome-again/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
 '''
 
+'''
+Given current Gnome issues this is causing the desktop env to crash
+'''
+
 from palin import getSubStrings
 import math
 
